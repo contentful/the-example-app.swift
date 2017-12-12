641397b712ba1d264ec46f0d9ae944d09a48d1e2
JP Wright <jp@contentful.com>
2017-12-12 13:50:04 +0100

    Highlighted course layout

--- the-example-app.swift/HomeLayout/TableViewCells/HighlightedCourseTableViewCell.swift
+++ the-example-app.swift/HomeLayout/TableViewCells/HighlightedCourseTableViewCell.swift
@@ -7,10 +7,20 @@ import Contentful
 class HighlightedCourseTableViewCell: UITableViewCell, CellConfigurable {
 
     func configure(item: HighlightedCourse) {
-        titleLabel.text = item.course?.title
-        descriptionLabel.text = item.course?.courseDescription
 
+        if let title = item.course?.title {
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.lineSpacing = 1.14
+            let attributedText = NSAttributedString(string: title, attributes: [.paragraphStyle: paragraphStyle])
+            titleLabel.attributedText = attributedText
+        }
 
+        if let description = item.course?.courseDescription {
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.lineSpacing = 1.27
+            let attributedText = NSAttributedString(string: description, attributes: [.paragraphStyle: paragraphStyle])
+            descriptionLabel.attributedText = attributedText
+        }
 
         guard let asset = item.course?.imageAsset else {
             // TODO: Set placeholder image
@@ -49,6 +59,40 @@ class HighlightedCourseTableViewCell: UITableViewCell, CellConfigurable {
             // TODO:
         }
 
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        viewCourseButton.layer.cornerRadius = viewCourseButton.frame.height / 2.0
+    }
+
+    @IBOutlet weak var containerView: UIView! {
+        didSet {
+            containerView.layer.masksToBounds = false
+            containerView.layer.cornerRadius = 15.0
+            containerView.layer.shadowRadius = 6.0
+            containerView.layer.shadowOpacity = 0.8
+            containerView.layer.shadowColor = UIColor(red: 0.83, green: 0.83, blue: 0.83, alpha: 1).cgColor
+            containerView.layer.shadowOffset = CGSize(width: 7.0, height: 7.0)
+        }
+    }
+
+    @IBOutlet weak var viewCourseButton: UIButton! {
+        didSet {
+            viewCourseButton.backgroundColor = UIColor(red: 0.95, green: 0.95, blue: 0.97, alpha: 1)
+            viewCourseButton.layer.cornerRadius = 20.0
+            viewCourseButton.titleLabel?.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
+            viewCourseButton.setTitleColor(UIColor(red: 0, green: 0.5, blue: 1, alpha: 1), for: .normal)
+        }
+    }
+
+    @IBOutlet weak var gettingStartedLabel: UILabel! {
+        didSet {
+            gettingStartedLabel.textColor = UIColor(white: 1.0, alpha: 0.5)
+            // TODO: Localize
+            gettingStartedLabel.text = "Getting Started".uppercased()
+            gettingStartedLabel.font = UIFont.systemFont(ofSize: 15.0, weight: .bold)
+        }
     }
 
     @IBOutlet weak var titleLabel: UILabel! {
