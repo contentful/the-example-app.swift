1366058c0e67fdd58de59114988d638ff626ad34
JP Wright <jp@contentful.com>
2018-02-02 10:45:25 +0100

    Debug crash when tapping inline markdown images

--- the-example-app.swift/Lessons/LessonModuleTableViewCells/LessonCopyTableViewCell.swift
+++ the-example-app.swift/Lessons/LessonModuleTableViewCells/LessonCopyTableViewCell.swift
@@ -26,5 +26,20 @@ class LessonCopyTableViewCell: UITableViewCell, CellConfigurable, UITextViewDele
         selectionStyle = .none
     }
 
-    @IBOutlet weak var copyTextView: UITextView!
+    @IBOutlet weak var copyTextView: UITextView! {
+        didSet {
+            copyTextView.delegate = self
+        }
+    }
+
+    // MARK: UITextViewDelegate
+
+    func textView(_ textView: UITextView, shouldInteractWith textAttachment: NSTextAttachment, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+        return false
+    }
+
+    func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+        UIApplication.shared.open(URL, options: [:], completionHandler: nil)
+        return false
+    }
 }
